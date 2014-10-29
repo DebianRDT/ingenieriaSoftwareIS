f8881dde7cd7e306eee17aac93c2d47d29821d98
Jose Molino Ortega <debianzonerdt@gmail.com>
2014-10-29 16:47:43 +0100

    Añadidos los dados

---
+++ dados.cpp
@@ -0,0 +1,26 @@
+#include "dados.h"
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <list>
+
+using namespace std;
+
+Dados::Dados(int caras)
+{
+   srand(time(NULL));
+   caras_=caras;
+}
+
+void Dados::lanzarDado(int cantidad) {
+    int i;
+    for (i=0; i<cantidad; i++) {
+      //  dado_=1+rand()%(caras_);
+        _tiradas.push_back(10);
+    }
+}
+
+list<int> Dados::listaTiradas() {
+    return _tiradas;
+}
